4d19370fb4dc20973e7787f10659c34e63367b7d
9mrcookie9 <kontakt.9mrcookie9@gmail.com>
2016-07-15 16:30:10 +0200

    Optimization and Fixes
    Fixed name error.
    Name of SystemMain var - "main" overriding int main function.

--- script/script.ino
+++ script/script.ino
@@ -1,21 +1,15 @@
 #include <math.h>
 #include <Wire.h>
 #include "System.h"
-#include "DhtUse.h"
-#include "LcdUse.h"
-#include "ButtonUse.h"
-#include "LedsUse.h"
-#include "NetworkUse.h"
-#include "PhotoresistorUse.h"
 
-SystemMain main;
+SystemMain logic;
 
-void setup(){
-    main.Init();
-    main.Setup();
+void setup() {
+    logic.Init();
+    logic.Setup();
 }
 
-void loop(){
-	_delay_ms(1);
-    main.Update();
+void loop() {
+    _delay_ms(1);
+    logic.Update();
 }
